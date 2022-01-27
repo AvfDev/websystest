77d5cbc546cd83be21fb87b218acd8eab0f24e4f
Anthony Vasquez <anth_dj87@hotmail.com>
2022-01-27 09:41:05 -0400

    Agregar archivos de proyecto.

---
+++ WebSys Test/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace WebSys_Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] values = { 1, 2, 3, 5, 6, 7, 8, 9 };
+            Console.WriteLine("Max Value is: {0}", GetMaxValue(values));
+            Console.WriteLine("Min Value is: {0}", GetMinValue(values));
+            Console.WriteLine("Avg Value is: {0}", GetAverageValueFromArray(values));
+            Console.WriteLine("Avg Value is: {0}", GetModalValue(values));
+        }
+
+        private static int GetMaxValue(int[] valuesToCalculate) {
+            var maxValue = 0;
+            foreach(var currentValue in valuesToCalculate) {
+                maxValue = currentValue > maxValue ? currentValue : maxValue;
+            }
+
+            return maxValue;
+        }
+
+        private static int GetMinValue(int[] valuesToCalc)
+        {
+            var minValue = 1;
+
+            foreach (var currentValue in valuesToCalc)
+            {
+                minValue = currentValue < minValue ? currentValue : minValue;
+            }
+            return minValue;
+        }
+
+        private static int GetAverageValueFromArray(int[] valuesToCalculate)
+        {
+            var totalSumOfValues = 0;
+
+            foreach (var currentValue in valuesToCalculate) {
+                totalSumOfValues += currentValue;
+            }
+
+            var averageValue = totalSumOfValues / valuesToCalculate.Length;
+            return averageValue;
+        }
+
+        private static decimal GetModalValue(int[] valuesToCalculate)
+        {
+            var arrayLength = valuesToCalculate.Length;
+
+            var result = ((decimal)valuesToCalculate[arrayLength / 2] + (decimal)valuesToCalculate[(arrayLength / 2) + 1]) / 2;
+
+            return result;
+        }
+    }
+}
